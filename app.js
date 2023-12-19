d25843ef654897c3954ff612ee8970b1a309aaff
rhitamsanyal <rhitamsanyal@gmail.com>
2023-12-19 15:43:05 -0800

    Add new msg

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require("http");
 
-http.createServer(function(req,res)
-	{
-		res.write("welcome to rsanyal.com!! :)");
-		res.end();
-
-
-	}).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("welcome to rsanyal.com!! :)");
+    res.write("still learning :)");
+    res.end();
+  })
+  .listen(3000);
