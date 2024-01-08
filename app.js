0a1e501c42fa2a27ca51ca8e10c24acf0a68f56f
rhitamsanyal <rhitamsanyal@gmail.com>
2024-01-07 17:16:14 -0800

    make some changes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write("welcome to rsanyal.com!! :)");
-    res.write("still learning :)");
+    res.write("welcome to rsanyal.com!! :)\n");
+    res.write("still learning :)\n");
+    res.write("still learning :) added some more!");
     res.end();
   })
   .listen(3000);
